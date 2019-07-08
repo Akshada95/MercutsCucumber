d468b2227df3ae7ed686a19c1314964c94ec483e
Akshada95 <sangale441@gmail.com>
2019-07-08 15:20:30 +0530

    Updated Cucumber

--- src/test/java/mt/mstr/steps/Loginsteps.java
+++ src/test/java/mt/mstr/steps/Loginsteps.java
@@ -35,7 +35,7 @@ public class Loginsteps
     		System.setProperty("webdriver.ie.driver", "F:\\Selenium Setup\\setup1\\4052019\\IEDriverServer_x64_3.14.0 (12)\\IEDriverServer.exe");
     		Config.driver=new InternetExplorerDriver();
     		System.out.println("IE browser is opened successfully");
-    		System.out.println("Git updated");
+    		System.out.println("Git updated in IE");
    
     	}else
     	{
