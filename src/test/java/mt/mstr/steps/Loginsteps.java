cbf293fa7a794634e72cd92877ce27dc19e271bb
Akshada95 <sangale441@gmail.com>
2019-07-08 14:20:44 +0530

    Updated Cucumber

--- src/test/java/mt/mstr/steps/Loginsteps.java
+++ src/test/java/mt/mstr/steps/Loginsteps.java
@@ -35,7 +35,8 @@ public class Loginsteps
     		System.setProperty("webdriver.ie.driver", "F:\\Selenium Setup\\setup1\\4052019\\IEDriverServer_x64_3.14.0 (12)\\IEDriverServer.exe");
     		Config.driver=new InternetExplorerDriver();
     		System.out.println("IE browser is opened successfully");
-    		System.out.println("IE is updated");
+    		System.out.println("Git updated");
+   
     	}else
     	{
     		System.setProperty("webdriver.gecko.driver","F:\\Selenium Setup\\setup1\\4052019\\geckodriver-v0.24.0-win64\\geckodriver.exe");
